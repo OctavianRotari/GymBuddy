f9c3856d9364587e5d4b79d49dc16e30ed99fa77
octavianrotari <octavianrotari@gmail.com>
2015-12-15 18:59:45 +0000

    #5 close

--- GymBuddy/www/js/log-in-controller.js
+++ GymBuddy/www/js/log-in-controller.js
@@ -1,12 +1,17 @@
-gymBuddyApp.controller('LogIn', function($scope) {
+gymBuddyApp.controller('LogInCtrl', function($scope) {
 
-	var ref = new Firebase("https://.firebaseio.com");
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
+  $scope.name = null;
+
+  $scope.login = function(){
+    var ref = new Firebase("https//luminous-torch-8195.firebaseio.com/users");
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        $scope.name = authData.facebook.displayName;
+        console.log("Authenticated successfully with payload:", $scope.username);
+      }
+    });
+  };
 
 });
