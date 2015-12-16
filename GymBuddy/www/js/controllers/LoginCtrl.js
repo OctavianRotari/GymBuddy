e2f60f3691188913be3df19e99463b8b24ed803f
octavianrotari <octavianrotari@gmail.com>
2015-12-16 14:47:38 +0000

    ref data into firebase

--- GymBuddy/www/js/controllers/LoginCtrl.js
+++ GymBuddy/www/js/controllers/LoginCtrl.js
@@ -3,14 +3,12 @@ gymBuddyApp.controller('LogInCtrl', function($scope, $state) {
 
   $scope.login = function(){
     var ref = new Firebase("https//luminous-torch-8195.firebaseio.com/users");
-    var refSave = new Firebase("https://luminous-torch-8195.firebaseio.com/users/");
     ref.authWithOAuthPopup("facebook", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
-        var usersRef = refSave.child("user");
 
-        usersRef.set({
+        ref.push({
           firstName: authData.facebook.cachedUserProfile.first_name,
           lastName: authData.facebook.cachedUserProfile.last_name,
           gender: authData.facebook.cachedUserProfile.gender,
