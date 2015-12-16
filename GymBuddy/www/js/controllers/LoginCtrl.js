204dfa410e5f5c56b4f3608b6a9a18a7d3f189d5
octavianrotari <octavianrotari@gmail.com>
2015-12-16 18:12:46 +0000

    15 close sign up with email and password

--- GymBuddy/www/js/controllers/LoginCtrl.js
+++ GymBuddy/www/js/controllers/LoginCtrl.js
@@ -1,9 +1,39 @@
 gymBuddyApp.controller('LogInCtrl', function($scope, $state) {
+  $scope.data = {firstName:null, lastName:null,email:null,password:null,confirmationPassword:null};
+  var ref = new Firebase("https//luminous-torch-8195.firebaseio.com/users");
+  var check = 0;
 
+  $scope.signUp = function(){
+    ref.createUser({
+        email:  $scope.data.email,
+        password:  $scope.data.password,
+    },
+
+    function(error, userData){
+      if(error){
+        switch(error.code){
+          case "EMAIL_TAKEN":
+            console.log("The new user account cannot be created because the email is already in use.");
+            break;
+          case "INVALID_EMAIL":
+            console.log("The specified email is not a valid email.");
+            break;
+          default:
+            console.log("Error creating user:", error);
+        }
+      } else {
+        ref.push({
+          firstName: $scope.data.firstName,
+          lastName:  $scope.data.lastName,
+          email:  $scope.data.email,
+        });
+        console.log("Successfully created user account with uid", userData.uid);
+        $state.go('tab.dash');
+      }
+    })
+  };
 
   $scope.login = function(){
-    var ref = new Firebase("https//luminous-torch-8195.firebaseio.com/users");
-    var check = 0;
     ref.authWithOAuthPopup("facebook", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
