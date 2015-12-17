7d0a66e2174d8df9605b2751d9de12458a795f03
NULL-OPERATOR <x@cryptolab.net>
2015-12-17 16:19:53 +0000

    close 14

--- GymBuddy/www/js/controllers/profileCtrl.js
+++ GymBuddy/www/js/controllers/profileCtrl.js
@@ -1,2 +1,8 @@
-gymBuddyApp.controller('ProfileCtrl', function($scope) {})
+gymBuddyApp.controller('ProfileCtrl', function($scope, $state) {
+  var ref = new Firebase("https//luminous-torch-8195.firebaseio.com/users");
 
+  $scope.signOut = function() {
+    ref.unauth();
+    $state.go('sign-in');
+  };
+})
