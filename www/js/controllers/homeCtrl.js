8e21aef20ace6133f838c3d7406f66a57a00074f
NULL-OPERATOR <x@cryptolab.net>
2015-12-24 13:31:55 +0000

    integrate new user service to home page

--- www/js/controllers/homeCtrl.js
+++ www/js/controllers/homeCtrl.js
@@ -1,23 +1,20 @@
-gymBuddyApp.controller('homeCtrl', ["$scope", "$state", "$firebaseObject", function($scope, $state, $firebaseObject) {
+gymBuddyApp.controller('homeCtrl', ["$scope",
+                                    "$state",
+                                    "$firebaseObject",
+                                    "profileData",
+                                    function($scope,
+                                             $state,
+                                             $firebaseObject,
+                                             profileData) {
+// stacking skilllz
+//...lol
 
+var getData = function(){
+  profileData.getData().then(function(thing) {
+    $scope.data = thing
+  });
+};
 
-  uniqueid = ''
-  var ref = new Firebase("https//luminous-torch-8195.firebaseio.com/users/-K5p0ukbIsVU2nLBoZwH");
-  $scope.obj = $firebaseObject(ref);
-  //
-  // obj.$loaded().then(function() {
-  //    console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-  //
-  //   // To iterate the key/value pairs of the object, use angular.forEach()
-  //   angular.forEach(obj, function(value, key) {
-  //      console.log(key, value);
-  //   });
-  // });
-  //
-  // // To make the data available in the DOM, assign it to $scope
-  // $scope.data = obj;
-  //
-  // // For three-way data bindings, bind it to the scope instead
-  // obj.$bindTo($scope, "data");
+  getData();
 
 }]);
