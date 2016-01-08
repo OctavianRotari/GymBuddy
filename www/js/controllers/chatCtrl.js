cb70162b38569472cd29adaf4a4d720fc4b840c1
octavianrotari <octavianrotari@gmail.com>
2016-01-08 15:29:33 +0000

    messages

--- www/js/controllers/chatCtrl.js
+++ www/js/controllers/chatCtrl.js
@@ -1,5 +1,5 @@
 angular.module('gymBuddy.controllers')
-.controller('chatCtrl', function(chatData, $rootScope, $scope, $stateParams, buddylist) {
+.controller('chatCtrl', function($firebaseArray, chatData, $rootScope, $scope, $stateParams, buddylist) {
 
   var ref = new Firebase(firebaseUrl);
 
@@ -15,11 +15,12 @@ angular.module('gymBuddy.controllers')
   $scope.sendMessage = function(msg, currentUser){
     var refOtherUser = new Firebase(firebaseUrl + "users/" + $rootScope.chat.user1)
     currentUser = ref.getAuth().uid;
-    var refChat = new Firebase(firebaseUrl +"rooms/" + chatId);
+    var refChat = new Firebase(firebaseUrl +"rooms/" + chatId + "/messages");
     var currentUser = refChat.getAuth().uid;
+    var messagesFire = $firebaseArray(refChat);
     var obj = {};
     obj[currentUser] = msg;
-    refChat.child("messages").set(obj);
+    messagesFire.$add(obj);
     var objReciver = {};
     var user = $rootScope.chat.user2;
     refOtherUser.child("chats").child($rootScope.chat.room).set({
