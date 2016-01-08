d7842887455e2d27a52d49198e6cc32d8950c4b6
octavianrotari <octavianrotari@gmail.com>
2016-01-08 15:33:58 +0000

    messages in the scope

--- www/js/controllers/chatCtrl.js
+++ www/js/controllers/chatCtrl.js
@@ -2,25 +2,23 @@ angular.module('gymBuddy.controllers')
 .controller('chatCtrl', function($firebaseArray, chatData, $rootScope, $scope, $stateParams, buddylist) {
 
   var ref = new Firebase(firebaseUrl);
-
+  var refChat = new Firebase(firebaseUrl +"rooms/" + chatId + "/messages");
   var chatId;
-
   var currentUser ;
+  var refOtherUser = new Firebase(firebaseUrl + "users/" + $rootScope.chat.user1)
 
+  $scope.messagesFire = $firebaseArray(refChat);
 
   var messages = function(){
     chatId = $stateParams.chatId;
   };
 
   $scope.sendMessage = function(msg, currentUser){
-    var refOtherUser = new Firebase(firebaseUrl + "users/" + $rootScope.chat.user1)
     currentUser = ref.getAuth().uid;
-    var refChat = new Firebase(firebaseUrl +"rooms/" + chatId + "/messages");
     var currentUser = refChat.getAuth().uid;
-    var messagesFire = $firebaseArray(refChat);
     var obj = {};
     obj[currentUser] = msg;
-    messagesFire.$add(obj);
+    $scope.messagesFire.$add(obj);
     var objReciver = {};
     var user = $rootScope.chat.user2;
     refOtherUser.child("chats").child($rootScope.chat.room).set({
@@ -28,5 +26,4 @@ angular.module('gymBuddy.controllers')
     });
   };
 
-  messages();
 })
